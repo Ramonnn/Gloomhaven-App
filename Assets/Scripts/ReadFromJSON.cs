043ac6f3ec3353ed4c4e864f1df41d81e342bcba
Ramonnn <ramon.eliens@gmail.com>
2018-03-07 21:53:53 +0100

    The App is now JSON conscious and can read the scenariolist. Communication with GameObjects still needs to be implemented.

---
+++ Assets/Scripts/ReadFromJSON.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ReadFromJSON : MonoBehaviour
+{
+
+    private string scenariosFileName = "scenarios.json";
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadGameData();
+    }
+
+    private void LoadGameData()
+    {
+        // Path.Combine combines strings into a file path
+        // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+        string filePath = Path.Combine(Application.streamingAssetsPath, scenariosFileName);
+
+        if (File.Exists(filePath))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(filePath);
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            ScenarioList loadedData = JsonUtility.FromJson<ScenarioList>(dataAsJson);
+            Debug.Log("I'm loading" + loadedData.scenarios[0].name);
+
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+}
+
+[System.Serializable]
+public class ScenarioList
+{
+    public List<Scenario> scenarios;
+}
+
+[System.Serializable]
+public class Scenario
+{
+    public string name;
+    public List<Monster> decks;
+}
+
+[System.Serializable]
+public class Monster
+{
+    public string name;
+}
