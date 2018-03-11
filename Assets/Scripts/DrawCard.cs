9c251b9bc389e96dbdd68f40a25a3a324812db71
Ramonnn <ramon.eliens@gmail.com>
2018-03-11 12:19:55 +0100

    JSON 1.0

--- Assets/Scripts/DrawCard.cs
+++ Assets/Scripts/DrawCard.cs
@@ -4,19 +4,25 @@ using System.Collections.Generic;
 
 public class DrawCard : MonoBehaviour
 {
-
     public Sprite cardFront, cardBack;
     private SpriteRenderer cardArt, shuffleIcon;
     private Card currentCard;
     public TextMeshPro initiative, monsterName, line1;
-    private List<Card> cardDeck = new List<Card>();
+    public List<Card> cardDeck = new List<Card>();
     private List<Card> discardPile = new List<Card>();
     private string objectName;
+    public List<Deck> deckList;
+    public List<Combinations> monsterDeckCombi;
 
     private void Start()
     {
+        deckList = GetComponentInParent<LoadScenario>().loadedDecks.decks;
+        monsterDeckCombi = GetComponentInParent<LoadScenario>().loadedMonsters.monsters;
         objectName = gameObject.name; //GetInstanceID() could work too?
-        cardDeck = GetComponentInParent<DeckCollection>().decklist[objectName];
+        Debug.Log(monsterDeckCombi[1].name + objectName);
+        DeckLoader();
+
+
         cardArt = gameObject.GetComponent<SpriteRenderer>();
 
         SpriteRenderer[] renderers = GetComponentsInChildren<SpriteRenderer>();
@@ -26,6 +32,26 @@ public class DrawCard : MonoBehaviour
             if (renderer.GetInstanceID() != GetComponent<SpriteRenderer>().GetInstanceID())
             {
                 shuffleIcon = renderer;
+            }
+        }
+    }
+
+    public void DeckLoader()
+    {
+        for (int i = 0; i < monsterDeckCombi.Count; i++)
+        {
+            if (objectName == monsterDeckCombi[i].name)
+            {
+                Debug.Log("Loading Deck for " + monsterDeckCombi[i].name);
+                string getMonsterClass = monsterDeckCombi[i].monsterclass;
+                for (int x = 0; x < deckList.Count; x++)
+                {
+                    if (getMonsterClass == deckList[x].monsterclass)
+                    {
+                        cardDeck = deckList[x].cards;
+                    }
+
+                }
             }
         }
     }
@@ -41,7 +67,7 @@ public class DrawCard : MonoBehaviour
 
             monsterName.text = objectName;
             initiative.text = currentCard.initiative;
-            line1.text = currentCard.ln1;
+            line1.text = currentCard.cardlines[0]; // for loop so every piece of information gets its own line.
 
             cardDeck.RemoveAt(topDeck);
             Debug.Log("Deck contains " + cardDeck.Count + " Cards");
--- Assets/Scripts/LoadScenario.cs
+++ Assets/Scripts/LoadScenario.cs
@@ -1,45 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using System.IO;
+using System;
 
 public class LoadScenario : MonoBehaviour
 {
+    private string scenariosFileName = "scenarios.json";
+    private string decksFileName = "decks.json";
+    private string monstersFileName = "monsters.json";
+    public ScenarioList loadedScenarios;
+    public DecksList loadedDecks;
+    public Monsters loadedMonsters;
 
     public string selectedScenario;
-    public Dictionary<string, string[]> scenarioList;
-    public string[] enemyNames;
+    public List<Scenario> scenarioList;
+    public List<Monster> enemyNames;
     public GameObject card, newDeck;
+    public string scenarioName;
 
-    // Use this for initialization
     private void Awake()
     {
-        scenarioList = GetComponent<ScenarioCollection>().scenarios;
+        LoadScenarioData();
+        LoadDeckData();
+        LoadCombinations();
     }
 
     void Start()
     {
-        DeckSpawner();
+        scenarioList = loadedScenarios.scenarios;
+        ScenarioLoader();
     }
 
-    public void DeckSpawner()
+    private void LoadScenarioData()
     {
-        selectedScenario = PlayerPrefs.GetString("ChosenScenario");
+        string filePath = Path.Combine(Application.streamingAssetsPath, scenariosFileName);
 
-        foreach (KeyValuePair<string, string[]> item in scenarioList)
+        if (File.Exists(filePath))
         {
-            if (selectedScenario == item.Key)
+            string dataAsJson = File.ReadAllText(filePath);
+            loadedScenarios = JsonUtility.FromJson<ScenarioList>(dataAsJson);
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    private void LoadDeckData() //could be combined with LoadScenarioData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, decksFileName);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            loadedDecks = JsonUtility.FromJson<DecksList>(dataAsJson);
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    private void LoadCombinations() //could be combined with LoadScenarioData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, monstersFileName);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            loadedMonsters = JsonUtility.FromJson<Monsters>(dataAsJson);
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    public void ScenarioLoader()
+    {
+        selectedScenario = PlayerPrefs.GetString("ChosenScenario"); // this needs to change from e.g. Scenario 1 to #1 Black Barrows.
+        foreach (Scenario item in scenarioList)
+        {
+            if (selectedScenario == item.name)
             {
-                Debug.Log("The Scenario is " + item.Key);
-                enemyNames = item.Value;
-                for (int i = 0; i < item.Value.Length; i++)
+                Debug.Log("The Scenario is " + item.name);
+                scenarioName = item.name;
+                enemyNames = item.decks;
+
+                for (int i = 0; i < enemyNames.Count; i++)
                 {
-                    Debug.Log("Spawning Deck For " + enemyNames[i]);
+                    Debug.Log("Spawning Deck For " + enemyNames[i].name);
                     newDeck = Instantiate(card, GameObject.Find("DeckSpawn" + (i + 1)).transform.position, Quaternion.identity) as GameObject;
                     newDeck.transform.parent = GameObject.Find("DeckSpawn" + (i + 1)).transform;
-                    newDeck.name = enemyNames[i];
+                    newDeck.name = enemyNames[i].name;
                 }
             }
         }
     }
+}
+
+// Load Scenario Data
+
+[System.Serializable]
+public struct ScenarioList
+{
+    public List<Scenario> scenarios;
+}
+
+[System.Serializable]
+public struct Scenario
+{
+    public string name;
+    public List<Monster> decks;
+}
+
+[System.Serializable]
+public struct Monster
+{
+    public string name;
+}
+
+//Load Deck Data
+
+[System.Serializable]
+public struct DecksList
+{
+    public List<Deck> decks;
+}
+
+[System.Serializable]
+public struct Deck
+{
+    public string monsterclass;
+    public List<Card> cards;
+}
+
+[System.Serializable]
+public struct Card
+{
+    public bool shuffle;
+    public string initiative;
+    public List<String> cardlines;
+}
+
+// Connect Monster with their respective decks
+
+[System.Serializable]
+public struct Monsters
+{
+    public List<Combinations> monsters;
+}
+
+[System.Serializable]
+public struct Combinations
+{
+    public string name;
+    public string monsterclass;
+
 }
