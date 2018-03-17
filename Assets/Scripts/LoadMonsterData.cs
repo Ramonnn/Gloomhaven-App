2006ca65e91c83bcd20eb08279b2f95b02ce7fda
Ramonnn <ramon.eliens@gmail.com>
2018-03-17 22:42:50 +0100

    Names and Static Stats hacked onto the enemyframes

--- Assets/Scripts/LoadMonsterData.cs
+++ Assets/Scripts/LoadMonsterData.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using SimpleJSON;
 using System.IO;
+using UnityEngine.UI;
 
 public class LoadMonsterData : MonoBehaviour {
 
@@ -37,6 +38,36 @@ public class LoadMonsterData : MonoBehaviour {
                 monsterAttri.Add(GetMonsterfromJSON(monstername, 1, "normal")["attributes"][i].ToString());
             }
 
+            List<string> genericStatsNormal = new List<string>();
+            genericStatsNormal.Add(monsterHealth.ToString());
+            genericStatsNormal.Add(monsterMove.ToString());
+            genericStatsNormal.Add(monsterAttack.ToString());
+            genericStatsNormal.Add(monsterRange.ToString());
+
+            transform.GetChild(5).GetComponentInChildren<Text>().text = genericStatsNormal[0] + "\n" + genericStatsNormal[1] + "\n" + genericStatsNormal[2] + "\n" + genericStatsNormal[3];
+
+
+
+            int monsterHealthElite = GetMonsterfromJSON(monstername, 1, "Elite")["health"]; //level and monstertype will eventually be defined by the player.
+            int monsterMoveElite = GetMonsterfromJSON(monstername, 1, "Elite")["move"];
+            int monsterAttackElite = GetMonsterfromJSON(monstername, 1, "Elite")["attack"];
+            int monsterRangeElite = GetMonsterfromJSON(monstername, 1, "Elite")["range"];
+            List<string> monsterAttriElite = new List<string>();
+            for (int i = 0; i < GetMonsterfromJSON(monstername, 1, "Elite")["attributes"].Count; i++)
+            {
+                monsterAttri.Add(GetMonsterfromJSON(monstername, 1, "Elite")["attributes"][i].ToString());
+            }
+
+            List<string> genericStatsElite = new List<string>();
+            genericStatsElite.Add(monsterHealthElite.ToString());
+            genericStatsElite.Add(monsterMoveElite.ToString());
+            genericStatsElite.Add(monsterAttackElite.ToString());
+            genericStatsElite.Add(monsterRangeElite.ToString());
+
+            transform.GetChild(6).GetComponentInChildren<Text>().text = genericStatsElite[0] + "\n" + genericStatsElite[1] + "\n" + genericStatsElite[2] + "\n" + genericStatsElite[3];
+
+
+
             foreach (Sprite image in monsterImages)
             {
                 if (image.name == "Horz-" + gameObject.name)
@@ -44,7 +75,6 @@ public class LoadMonsterData : MonoBehaviour {
                     Debug.Log("found Image");
                     transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = image; //Good alternative for "Find" method. Or use a game manager dictionary containing all instantiations? and search by key...
                     currentMonsterImage = image; //Image spawn in enemyframe
-
                 }
             }
         }
